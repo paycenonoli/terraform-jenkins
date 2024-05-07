a28387136af3e95af4606d6d1ddddac493f6fbb1
paycenonoli <119795130+paycenonoli@users.noreply.github.com>
2024-05-06 21:46:12 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,12 +9,12 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
-# Create a VPC
+# Create an ec2 instance
 resource "aws_instance" "web_server" {
-  ami           = "ami-0440d3b780d96b29d" # Replace with the desired AMI ID
+  ami           = "ami-0ddda618e961f2270" # Replace with the desired AMI ID
   instance_type = var.ec2_instance_type
   tags = {
     Name = "web-server"
